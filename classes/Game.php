92ff6f4ab0503c610d9d9d89b44d29eae5282c4d
DimitriDR <dimitridroeck@gmail.com>
2023-01-06 11:27:13 +0100

    Ajout tests + classe Game.php
    Signed-off-by: DimitriDR <dimitridroeck@gmail.com>

--- classes/Game.php
+++ classes/Game.php
@@ -1,9 +1,45 @@
 <?php
+require_once "classes/Player.php";
 
 class Game
 {
     /**
      * @var array Liste des joueurs présents dans la partie
-     */
+    **/
     private array $players = [];
+
+    /**
+     * Constructeur permettant d'intégrer directement une liste de joueurs
+     * @param array $players Liste initiale des joueurs
+    **/
+    public function __construct(array $players)
+    {
+        $this->players = $players;
+    }
+
+    /**
+     * Fonction pour ajouter un joueur après que l'objet ait été créé
+     * @param Player $player
+     * @return void
+    **/
+    public function add_player(Player $player): void
+    {
+        $this->players[] = $player;
+    }
+
+    /**
+     * Fonction pour récupérer un objet Player à un emplacement du tableau via $id
+     * @param int $id Joueur à récupérer
+     * @return Player Joueur trouvé
+     * @throws InvalidArgumentException Si $id dépasse les bornes du tableau
+    **/
+    public function get_player_at(int $id): Player
+    {
+        if (($id >= 0) && ($id < sizeof($this->players)))
+        {
+            return $this->players[$id];
+        } else {
+            throw new InvalidArgumentException("L'ID dépasse le nombre de joueurs présents");
+        }
+    }
 }
