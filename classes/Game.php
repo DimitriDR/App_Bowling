8398d63dcb11277eb38fde264d490b3fd751eb9d
lina-bkd <96200016+lina-bkd@users.noreply.github.com>
2023-01-06 15:55:28 +0100

    Implémentation des classes et des méthodes

--- classes/Game.php
+++ classes/Game.php
@@ -1,9 +1,126 @@
 <?php
 
+require_once "Player.php";
+require "Round.php";
+
 class Game
 {
     /**
      * @var array Liste des joueurs présents dans la partie
-     */
+    **/
     private array $players = [];
+
+    /**
+     * @var int Numéro du round actuel
+     * @note Le premier round est le round 1
+     * @note Le dernier round est le round 10
+     * @note Le round 11 est le round bonus : il est utilisé uniquement si le joueur fait un strike ou un spare au round 10
+     */
+    private int $current_round = 1;
+
+    /**
+     * @var int Numéro du joueur actuel
+     * @note Le premier joueur est le joueur 1
+     * @note Le dernier joueur est le joueur sizeof($this->players)
+     */
+    private int $current_player = 1;
+
+    /**
+     * Fonction pour récupérer le numéro du round actuel
+     * @return int
+     */
+    public function get_current_round(): int
+    {
+        return $this->current_round;
+    }
+
+    /**
+     * Fonction pour récupérer le numéro du joueur actuel
+     * @return int
+     */
+    public function get_current_player(): int
+    {
+        return $this->current_player;
+    }
+
+    /**
+     * Fonction pour mettre à jour le numéro du round actuel
+     * @param int $round Nouveau numéro de round
+     * @return void
+     * @throws InvalidArgumentException Si le numéro de round est inférieur à 1 ou supérieur à 11
+     */
+    public function set_current_round(int $round): void
+    {
+        if (($round < 1) || ($round > 11)) {
+            throw new InvalidArgumentException("Le numéro de round doit être compris entre 1 et 11");
+        }
+
+        $this->current_round = $round;
+    }
+
+    /**
+     * Fonction pour mettre à jour le numéro du joueur actuel
+     * @param int $player Nouveau numéro de joueur
+     * @return void
+     * @throws InvalidArgumentException Si le numéro de joueur est inférieur à 1 ou supérieur à sizeof($this->players)
+     */
+    public function set_current_player(int $player): void
+    {
+        if (($player < 1) || ($player > sizeof($this->players))) {
+            throw new InvalidArgumentException("Le numéro de joueur doit être compris entre 1 et " . sizeof($this->players));
+        }
+
+        $this->current_player = $player;
+    }
+
+    /**
+     * Constructeur permettant d'intégrer directement une liste de joueurs
+     * @param array $players Liste initiale des joueurs
+    **/
+    public function __construct(array $players)
+    {
+        $this->players = $players;
+    }
+
+    /**
+     * Fonction pour ajouter un joueur après que l'objet ait été créé
+     * @param Player $player
+     * @return void
+    **/
+    public function add_player(Player $player): void
+    {
+        $this->players[] = $player;
+    }
+
+    /**
+     * Fonction pour récupérer un objet Player à un emplacement du tableau via $id
+     * @param int $id Joueur à récupérer
+     * @return Player Joueur trouvé
+     * @throws InvalidArgumentException Si $id dépasse les bornes du tableau
+    **/
+    public function get_player_at(int $id): Player
+    {
+        if (($id >= 0) && ($id < sizeof($this->players)))
+        {
+            return $this->players[$id];
+        } else {
+            throw new InvalidArgumentException("L'ID dépasse le nombre de joueurs présents");
+        }
+    }
+
+    /**
+     * Fonction pour démarrer une partie
+     * @return void
+     * @throws InvalidArgumentException Si la partie a déjà été démarrée
+     */
+    public function start(): void
+    {
+        if ($this->current_round != 1) {
+            throw new InvalidArgumentException("La partie a déjà été démarrée");
+        }
+
+        $this->current_round = 1;
+        $this->current_player = 1;
+    }
+
 }
--- classes/Round.php
+++ classes/Round.php
@@ -1,6 +1,9 @@
 <?php
 
-class Round
+require 'Players.php';
+require 'Game.php';
+
+class Round 
 {
     /**
      * @var int Valeur du premier lancer
@@ -8,8 +11,55 @@ class Round
     private int $first_throw;
     /**
      * @var int Valeur du second lancer
+     * @note Si le joueur fait un strike, il n'a pas le droit au second lancer
      **/
     private int $second_throw;
+
+    /**
+     * @var int Valeur du troisième lancer
+     * @note Utilisé uniquement pour le dernier round
+     * @note Si le joueur fait un strike ou un spare, il a droit à un troisième lancer
+     */
+    private int $third_throw;
+
+    /**
+     * @var int Numéro du tour
+     * @note Le premier tour est le tour 1
+     * @note Le dernier tour est le tour 10
+     */
+    private int $turn;
+
+    /**
+     * Constructeur de la classe Round
+     * @param array $round_data Tableau contenant les valeurs des lancers
+     * @param int $turn Numéro du tour
+     * @throws InvalidArgumentException Si le tableau contient plus de 3 valeurs
+     */
+    public function __construct(array $round_data, int $turn)
+    {
+        if ($turn < 1 || $turn > 10) {
+            throw new InvalidArgumentException("Le numéro de tour doit être compris entre 1 et 10");
+        }
+
+        if($round_data.length() > 2 && $turn != 10)
+        {
+            throw new InvalidArgumentException("Le tableau contient plus de 2 valeurs pour un tour différent du 10");
+        }
+
+        if($round_data.length() > 3 && $turn == 10)
+        {
+            throw new InvalidArgumentException("Le tableau contient plus de 3 valeurs pour le tour 10");
+        }
+
+        if($turn == 10)
+        {
+            $this->third_throw = $round_data[2];
+        }
+
+        $this->first_throw = $round_data[0];
+        $this->second_throw = $round_data[1];
+        $this->turn = $turn;
+    }
 
     /**
      * Assesseur pour avoir le premier lancer
@@ -54,6 +104,91 @@ class Round
             throw new InvalidArgumentException("La valeur du lancer doit être comprise entre 0 et 10");
         }
 
+        if($this->first_throw + $second_throw > 10)
+        {
+            throw new InvalidArgumentException("La somme des deux lancers ne peut pas être supérieure à 10");
+        }
+
+        if($this->first_throw == 10)
+        {
+            throw new InvalidArgumentException("Le joueur a fait un strike, il n'a pas le droit au second lancer");
+        }
+
         $this->second_throw = $second_throw;
     }
+
+    /**
+     * Assesseur pour avoir le troisième lancer
+     * @return int
+     */
+    public function getThirdThrow(): int
+    {
+        return $this->third_throw;
+    }
+
+    /**
+     * Mutateur pour le troisième lancer
+     * @param int $third_throw
+     * @throws InvalidArgumentException
+     */
+    public function setThirdThrow(int $third_throw): void
+    {   
+        if ($third_throw < 0 || $third_throw > 10) {
+            throw new InvalidArgumentException("La valeur du lancer doit être comprise entre 0 et 10");
+        }
+
+        if($this->turn != 10)
+        {
+            throw new InvalidArgumentException("Le troisième lancer n'est utilisé que pour le dernier tour");
+        }
+
+        if($this->first_throw + $this->second_throw < 10)
+        {
+            throw new InvalidArgumentException("Le troisième lancer n'est utilisé que si le joueur fait un strike ou un spare");
+        }
+
+        $this->third_throw = $third_throw;
+    }
+
+    /**
+     * Fonction qui permet de savoir si le joueur a fait un strike
+     * @return bool
+     */
+    public function isStrike(): bool
+    {
+        if($this->first_throw === 10){
+            $this->setSecondThrow(0);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Fonction qui permet de savoir si le joueur a fait un spare
+     * @return bool
+     */
+
+    public function isSpare(): bool
+    {
+        return $this->first_throw + $this->second_throw === 10;
+    }
+
+    /**
+     * Fonction qui retourne le score du round
+     * @return array
+     */
+    public function getScore(): array
+    {
+        return [$this->first_throw, $this->second_throw];
+    }
+
+    /**
+     * Fonction qui attribue le score du round au joueur
+     * @param Players $player
+     */
+    public function setScore(Players $player): void
+    {
+        $player->set_marked_points($this->getScore());
+    }
+
 }
