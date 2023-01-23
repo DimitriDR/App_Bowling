d5533e797f40bbb70399808bc6ae7e5c9bebcddf
DimitriDR <dimitridroeck@gmail.com>
2023-01-23 23:21:00 +0100

    Correction petite faille
    Signed-off-by: DimitriDR <dimitridroeck@gmail.com>

--- controllers/player_name.php
+++ controllers/player_name.php
@@ -8,36 +8,35 @@ if (!isset($_POST["submit"]))
     header("Location: /");
 }
 
-// Vérification que tous les champs soient passés un par un
-
 $game_data = $_SESSION["game_data"];
 
-for ($i = 0; $i < $game_data["player_number"] ; $i++)
+// Vérification que tous les champs soient passés un par un
+for ($i = 0; $i < $game_data["player_number"]; $i++)
 {
-    if (!(isset($_POST["player_name_".$i])))
+    if (!(isset($_POST["player_name_" . $i])))
     {
         $_SESSION["error_message"] = "Le nom d'un joueur est manquant. Veuillez réessayer.";
         header("Location: /views/player_name.php");
         break;
-    } else {
-        continue;
     }
 }
 
-// Si tous les champs sont OK, on crée un objet pour chacun des joueurs renseignés
-for ($j = 0; $j < $game_data["player_number"] ; $j++)
-{
-    // On vérifie qu'on n'a pas plus de joueurs que ceux déclarés (si l'utilisateur retourne sur le formulaire par ex.)
-    $current_player_object_number = sizeof($game_data["players"]);
-    if (($current_player_object_number + 1) > $current_player_object_number)
-    {
-        break;
-    }
+// Vérification que l'on ne réinsère pas plus de joueurs que ce que le jeu a comme informations.
+// On sait que l'utilisateur veut essayer d'en rajouter si au moins un joueur peuple le tableau players
+if (isset($game_data["players"])) {
+    header("Location: /");
+    exit(0);
+}
 
+// Si tous les champs sont OK, on crée un objet pour chacun des joueurs renseignés
+for ($j = 0; $j < $game_data["player_number"]; $j++)
+{
     // Création du nouveau joueur que l'on va immédiatement rajouter dans le jeu
     $new_player = new Player($_POST["player_name_" . $j]);
     $game_data["players"][] = serialize($new_player);
 }
+
+$_SESSION["game_data"] = $game_data;
 
 header("Location: /");
 exit(0);
